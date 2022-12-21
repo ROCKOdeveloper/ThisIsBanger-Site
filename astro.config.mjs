1a074f5e69d66a1780a8b2a11699e742c2813d00
ROCKOraboloco <rockoraboloco@gmail.com>
2022-12-21 17:24:11 -0500

    build

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://ROCKOdeveloper.github.io',
+  base: '/ThisIsBanger-Site',
 });

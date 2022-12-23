fd30817a1af79b807d1cfe6a97589f003171cbdb
ROCKOraboloco <rockoraboloco@gmail.com>
2022-12-23 11:06:52 -0500

    error 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://ROCKOdeveloper.github.io',
-  base: '/ThisIsBanger-Site',
+  integrations: [tailwind()]
 });
